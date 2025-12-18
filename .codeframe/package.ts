1320c7b3031b5b7a01888e4d3a9da61842c90285
xsandre-l <ahloubser12@gmail.com>
2025-12-18 08:16:03 +0200

    .

--- .codeframe/package.ts
+++ .codeframe/package.ts
@@ -1,18 +1,34 @@
-import { BuildType, OUTPUT_DIR } from "../../../src/types/package-config.ts";
-import { runPackageAction } from "../../../src/commands/packages.ts";
+// import {
+//   BuildType,
+//   OUTPUT_DIR,
+// } from "../../../../src/core/types/package-config.ts";
+// import { runPackageAction } from "../../../../src/commands/packages.ts";
 
-import { resolve, join } from "node:path";
+import {
+  BuildType,
+  CPP_OUTPUT_DIR,
+  runPackageAction,
+  CMAKE_TOOLS,
+  getHostSysrootPath,
+  SYSROOT,
+} from "../../../../src/providers/package.privider.ts";
+
+import { join } from "node:path";
 import { argv } from "node:process";
 
 export const build = (cwd: string = process.cwd()): BuildType => {
-  const TOOLCHAINS = resolve(cwd, "../../toolchains/cmake-tools");
-  const LINUX = resolve(cwd, "../../toolchains/linux");
-  const toolchain = resolve(cwd, "../../toolchains/llvm-mingw");
-  const CLANG = join(toolchain, "bin/clang.exe").replace(/\\/g, "/");
-  const CLANGXX = join(toolchain, "bin/clang++.exe").replace(/\\/g, "/");
-  const WINDRES = join(toolchain, "bin/llvm-windres.exe").replace(/\\/g, "/");
+  const { windows_x86_64, windows_aarch64, linux_x86_64, linux_aarch64 } =
+    SYSROOT;
+
+  const HOST_SYSROOT = getHostSysrootPath();
+  const CLANG = join(HOST_SYSROOT, "bin/clang.exe").replace(/\\/g, "/");
+  const CLANGXX = join(HOST_SYSROOT, "bin/clang++.exe").replace(/\\/g, "/");
+  const WINDRES = join(HOST_SYSROOT, "bin/llvm-windres.exe").replace(
+    /\\/g,
+    "/"
+  );
   const AARCH64_WINDRES = join(
-    toolchain,
+    HOST_SYSROOT,
     "bin/aarch64-w64-mingw32-windres.exe"
   ).replace(/\\/g, "/");
 
@@ -32,7 +48,7 @@ export const build = (cwd: string = process.cwd()): BuildType => {
       -DCMAKE_CXX_COMPILER_TARGET=x86_64-w64-windows-gnu \
       -DCMAKE_SYSTEM_NAME=Windows \
       -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/zlib/windows/x86_64
+      -DCMAKE_INSTALL_PREFIX=${CPP_OUTPUT_DIR}/zlib/windows/x86_64
     `,
 
       buildStep: `cmake --build build/windows/x86_64 -j`,
@@ -53,14 +69,14 @@ export const build = (cwd: string = process.cwd()): BuildType => {
       -DCMAKE_CXX_COMPILER_TARGET=aarch64-w64-windows-gnu \
       -DCMAKE_SYSTEM_NAME=Windows \
       -DCMAKE_SYSTEM_PROCESSOR=aarch64 \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/zlib/windows/aarch64
+      -DCMAKE_INSTALL_PREFIX=${CPP_OUTPUT_DIR}/zlib/windows/aarch64
       `,
       buildStep: `cmake --build build/windows/aarch64 -j`,
       installStep: `cmake --install build/windows/aarch64`,
     },
     linux_x86_64: {
       configStep: `cmake -S . -B build/linux/x86_64 -G Ninja \
-      -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAINS}/linux_x86-64.cmake \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLS}/linux_x86-64.cmake \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=${CLANG} \
       -DCMAKE_CXX_COMPILER=${CLANGXX} \
@@ -70,7 +86,7 @@ export const build = (cwd: string = process.cwd()): BuildType => {
       -DBUILD_SHARED_LIBS=ON \
       -DZLIB_BUILD_MINIZIP=OFF \
       -DZLIB_BUILD_TESTING=OFF \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/zlib/linux/x86_64 
+      -DCMAKE_INSTALL_PREFIX=${CPP_OUTPUT_DIR}/zlib/linux/x86_64 
       `,
 
       buildStep: `cmake --build build/linux/x86_64 -j`,
@@ -78,7 +94,7 @@ export const build = (cwd: string = process.cwd()): BuildType => {
     },
     linux_aarch64: {
       configStep: `cmake -S . -B build/linux/aarch64 -G Ninja \
-      -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAINS}/linux_aarch64.cmake \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLS}/linux_aarch64.cmake \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=${CLANG} \
       -DCMAKE_CXX_COMPILER=${CLANGXX} \
@@ -88,7 +104,7 @@ export const build = (cwd: string = process.cwd()): BuildType => {
       -DBUILD_SHARED_LIBS=ON \
       -DZLIB_BUILD_MINIZIP=OFF \
       -DZLIB_BUILD_TESTING=OFF \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/zlib/linux/aarch64
+      -DCMAKE_INSTALL_PREFIX=${CPP_OUTPUT_DIR}/zlib/linux/aarch64
       `,
       buildStep: `cmake --build build/linux/aarch64 -j`,
       installStep: `cmake --install build/linux/aarch64`,
